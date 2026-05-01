b749b3bae5a6f706ae045506db4814848b6013a6
Rain <346731704@qq.com>
2026-05-01 16:12:35 +0800

    modify config of neovide

--- lua/neovide.lua
+++ lua/neovide.lua
@@ -5,10 +5,20 @@ vim.g.neovide_padding_bottom = 5
 vim.g.neovide_padding_right = 5
 vim.g.neovide_padding_left = 5
 
-vim.g.neovide_opacity = 1
-vim.g.neovide_normal_opacity = 1
+vim.g.neovide_opacity = 0.8
+vim.g.neovide_normal_opacity = 0.8
 
-vim.g.neovide_floating_blur_amount_x = 0
-vim.g.neovide_floating_blur_amount_y = 0
+vim.g.neovide_floating_blur_amount_x = 0.5
+vim.g.neovide_floating_blur_amount_y = 0.5
 vim.g.neovide_floating_shadow = false
-vim.g.neovide_floating_corner_radius = 0.25
+vim.g.neovide_floating_corner_radius = 0.5
+
+vim.g.neovide_title_background_color = string.format(
+    "%x",
+    vim.api.nvim_get_hl(0, {id=vim.api.nvim_get_hl_id_by_name("Normal")}).bg
+)
+
+vim.g.neovide_title_text_color = string.format(
+    "%x",
+    vim.api.nvim_get_hl(0, {id=vim.api.nvim_get_hl_id_by_name("Normal")}).fg
+)
